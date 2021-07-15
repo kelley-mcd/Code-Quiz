cb0356f163befd7aae040b9bb88d821d0859313e
kelley-mcd <kmcdonnell8@gmail.com>
2021-07-15 18:40:04 -0400

    commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let questionBank = [
+    {
+        question: "answer this question?",
+        answers: {
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D"
+        },
+        answer: "C"
+    },
+    {
+        question: "answer this question?",
+        answers: {
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D"
+        },
+        answer: "C"
+    },
+    {
+        question: "answer this question?",
+        answers: {
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D"
+        },
+        answer: "C"
+    },
+    {
+        question: "answer this question?",
+        answers: {
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D"
+        },
+        answer: "C"
+    },
+    {
+        question: "answer this question?",
+        answers: {
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D"
+        },
+        answer: "C"
+    },
+]
+
