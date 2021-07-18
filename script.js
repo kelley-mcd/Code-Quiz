f22ad5b05d020cc79fd01462502792b19b2daef9
kelley-mcd <kmcdonnell8@gmail.com>
2021-07-18 14:36:22 -0400

    commit

--- script.js
+++ script.js
@@ -1,84 +1,87 @@
-// select all elements
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
 const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
+const timeGauge = document.getElementById("timer");
 const scoreDiv = document.getElementById("scoreContainer");
+const finalScore = document.getElementById("finalScore");
 
-// create our questions
 let questions = [
     {
-        question : "What does HTML stand for?",
-        imgSrc : "img/html.png",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    },{
-        question : "What does CSS stand for?",
-        imgSrc : "img/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },{
-        question : "What does JS stand for?",
-        imgSrc : "img/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
+        question : "Commonly used data types DO NOT include:",
+        choiceA : "Strings",
+        choiceB : "Booleans",
+        choiceC : "Alerts",
+        choiceD: "Numbers",
         correct : "C"
+    },{
+        question : "The condition in an if/else statement is enclosed with _________.",
+        choiceA : "Quotes",
+        choiceB : "Curly brackets",
+        choiceC : "Parentheses",
+        choiceD : "Square brackets",
+        correct : "C"
+    },{
+        question : "Arrays in JavaScript can be used to store __________.",
+        choiceA : "Numbers and Strings",
+        choiceB : "Other Arrays",
+        choiceC : "Booleans",
+        choiceD : "All of the Above",
+        correct : "D"
+    },{
+        question : "String values must be enclosed with ______ when being assigned to variables.",
+        choiceA : "Commas",
+        choiceB : "Curly brackets",
+        choiceC : "Quotes",
+        choiceD : "parentheses",
+        correct : "C"  
     }
 ];
 
-// create some variables
 
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
+const questionTime = 15;
+const gaugeWidth = 150; 
 const gaugeUnit = gaugeWidth / questionTime;
 let TIMER;
 let score = 0;
 
-// render a question
-function renderQuestion(){
+
+function getQuestion(){
     let q = questions[runningQuestion];
     
     question.innerHTML = "<p>"+ q.question +"</p>";
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
 }
 
 start.addEventListener("click",startQuiz);
 
-// start quiz
-function startQuiz(){
+
+function startQuiz(e){
+    e.preventDefault();
     start.style.display = "none";
-    renderQuestion();
+    getQuestion();
     quiz.style.display = "block";
     renderProgress();
     renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    TIMER = setInterval(renderCounter,1000); 
 }
 
-// render progress
 function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    for(let i = 0; i <= lastQuestion; i++){
+        progress.innerHTML += "<div class='prog' id="+ i +"></div>";
     }
 }
 
-// counter render
 
 function renderCounter(){
     if(count <= questionTime){
@@ -87,67 +90,49 @@ function renderCounter(){
         count++
     }else{
         count = 0;
-        // change progress color to red
         answerIsWrong();
         if(runningQuestion < lastQuestion){
             runningQuestion++;
-            renderQuestion();
+            getQuestionn();
         }else{
-            // end the quiz and show the score
             clearInterval(TIMER);
-            scoreRender();
+            getScore();
         }
     }
 }
 
-// checkAnwer
 
 function checkAnswer(answer){
     if( answer == questions[runningQuestion].correct){
-        // answer is correct
         score++;
-        // change progress color to green
         answerIsCorrect();
     }else{
-        // answer is wrong
-        // change progress color to red
         answerIsWrong();
     }
     count = 0;
     if(runningQuestion < lastQuestion){
         runningQuestion++;
-        renderQuestion();
+        getQuestion();
     }else{
-        // end the quiz and show the score
         clearInterval(TIMER);
-        scoreRender();
+        getScore();
     }
 }
 
-// answer is correct
+
 function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+    document.getElementById(runningQuestion).style.backgroundColor = "black";
 }
 
-// answer is Wrong
+
 function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+    document.getElementById(runningQuestion).style.backgroundColor = "black";
 }
 
-// score render
-function scoreRender(){
+
+
+function getScore(){
+    
     scoreDiv.style.display = "block";
-    
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
-    
-    // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
-              "img/1.png";
-    
-    scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+  
 }
